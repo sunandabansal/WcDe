3ea811894d3e8985d2fd6e6978dff449a3a19144
Sunanda Bansal <mitali.cyrus@gmail.com>
2021-05-24 20:28:47 -0400

    Removed argparse, makes debugging difficult

--- demo.py
+++ demo.py
@@ -9,11 +9,8 @@ Year    :   2021
 
 # Importing Libraries
 import os      
-import pdb 
-import argparse
-import numpy as np
-import pandas as pd
 
+# Importing Demo Modules
 import helpers
 import WcDe
 
@@ -78,82 +75,35 @@ def read_glove_embeddings(path, vocab, vector_size):
         Pandas Series with words as the index and vector (numpy array) as the corresponding value.
 
     '''
-    embeddings = {}
+    words_found = []
+    embeddings  = []
     
     with open(os.path.expanduser(path)) as file:
         for line in file:
-            word    = " ".join(line.split()[:-vector_size]) 
-            vector  = line.split()[-vector_size:]
+            word    = line.split()[0] 
+            vector  = line.split()[1:]
             
             if vocab is None or word in vocab:
-                embeddings[word] = vector    
-
-    embeddings = pd.DataFrame.from_dict(embeddings, orient="index").astype(float)
-
-    embeddings = embeddings.apply(np.asarray, axis=1).rename("vector")
+                words_found.append(word)
+                embeddings.append(vector) 
     
-    embeddings = embeddings.reindex(vocab).dropna()
-    
-    embeddings.index.name = "word"
-    
-    return embeddings
+    return words_found, embeddings
         
-def parse_command_line_arguments(desc=""):
-
-    parser = argparse.ArgumentParser(
-        description=desc,
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    parser.add_argument(
-                            '--dataset_path', 
-                            type=str, 
-                            help='Path to BBC datasets - path/to/bbc[sport]/'
-                       )
-
-    parser.add_argument(
-                            '--embedding_file', 
-                            type=str,
-                            help='Path to GloVe 100d file - /path/to/glove.6B.100d.txt'
-                       )
-
-    parser.add_argument(
-                            '--clustering_algorithm', 
-                            type=str,
-                            default="ahc",
-                            help='''Only "kmeans" and "ahc" supported.'''
-                       )
-
-    parser.add_argument(
-                            '--distance_threshold', 
-                            type=float,
-                            default=8.0,
-                            help='''AHC Distance Threshold'''
-                       )
-
-    parser.add_argument(
-                            '--linkage', 
-                            type=str,
-                            default="ward",
-                            help='''"average", "complete" or "ward"'''
-                       )
-
-    parser.add_argument(
-                            '--n_clusters', 
-                            type=int,
-                            default=None,
-                            help='''AHC or K-Means number of clusters'''
-                       )
-
-    return parser.parse_args()
 
 if __name__ == "__main__":
 
     # Read demo variables
-    args = parse_command_line_arguments()
+    dataset_path    = "/path/to/bbc"
+    embedding_file  = "/path/to/glove.6B.100d.txt"
+    
+    clustering_algorithm    = "ahc"
+    linkage                 = "ward"
+    n_clusters              = None
+    distance_threshold      = 8
     
     # Read Dataset
     print("Reading dataset.")
-    texts, classes = read_bbc_dataset(path=args.dataset_path)       
+    texts, classes = read_bbc_dataset(path=dataset_path)       
     
     # Tokenized the texts
     print("Tokenizing documents.")
@@ -164,30 +114,34 @@ if __name__ == "__main__":
     
     # Get word vectors (pandas.Series)
     print("Getting word vectors.")
-    word_vectors = read_glove_embeddings(path=args.embedding_file, vocab=vocab, vector_size=100)
+    words, word_vectors = read_glove_embeddings(path=embedding_file, vocab=vocab, vector_size=100)
     
     # Cluster Word Vectors
     print("Clustering word vectors.")
     cluster_labels = WcDe.cluster_word_vectors(
                                                     word_vectors=word_vectors,
-                                                    clustering_algorithm=args.clustering_algorithm,
-                                                    n_clusters=args.n_clusters,
-                                                    distance_threshold=args.distance_threshold,
-                                                    linkage=args.linkage
+                                                    clustering_algorithm=clustering_algorithm,
+                                                    n_clusters=n_clusters,
+                                                    distance_threshold=distance_threshold,
+                                                    linkage=linkage
                                               )
 
     # Generate Document Vectors
     print("Generating document vectors.")
     wcde_doc_vectors = WcDe.get_document_vectors(    
                                                     tokenized_texts,
-                                                    word_vectors=word_vectors,
+                                                    words=words,
                                                     cluster_labels=cluster_labels,
                                                     weight_function="cfidf",
                                                     normalize=True
                                                 )
+    
     # Task - Cluster Documents
     print("Clustering document vectors.")
-    document_clustering_model = sklearn.cluster.KMeans(n_clusters=5, random_state=0)
+    document_clustering_model = sklearn.cluster.KMeans(
+                                                            n_clusters=5, 
+                                                            random_state=3
+                                                       )
     document_clustering_model = document_clustering_model.fit(wcde_doc_vectors)
     document_cluster_label = list(document_clustering_model.labels_)
     
