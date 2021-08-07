Created variables to parameterize weighting sceme and length normalization. Fixed documentation.

WcDe.py
@@ -88,116 +88,45 @@ def cluster_word_vectors(
 
     return labels
 
-def get_document_vectors(
+
+def get_document_vectors(    
     tokenized_texts, 
     words,
     cluster_labels, 
     weight_function="cfidf", 
-    normalize=True
-    ):
-    
-    # Quick Fix (#9)
-    if type(tokenized_texts) != pd.core.series.Series:
-        tokenized_texts = pd.Series(tokenized_texts)
-
-    # Prepare dataframe with word vector and cluster labels for each word (index)
-    wec_df = pd.DataFrame(index=words)    
-    wec_df["label"] = cluster_labels    # Add Cluster labels to the dataframe    
-    wec_df.index.name = "word"          # Name the index
-
-    # Total number of texts (used in cfidf weight calculations)
-    N = len(tokenized_texts)           
-    
-    # Calculate cluster document frequency
-    '''
-        Required for CF-iDF cluster weight computation
-        
-        cf(i,j)  = count of every occurrence of any terms of cluster i that are present in document j
-        cdf(i,j) = cluster document frquency of cluster i in document j
-                 = number of times any term from cluster i appeared in document j
-    '''
-    # Prepare an index of term occurrence in documents
-    # Inverted Index - word, document, term frequency, cluster label
-    inverted_index = pd.DataFrame()
-    for idx, tokenized_text in enumerate(tokenized_texts):
-        if tokenized_text:
-
-            # Get terms and frequencies from tokenized text
-            tf = pd.Series(tokenized_text, name="term_freq").value_counts()
-
-            # Filter Word Cluster index (wec_df) to contain only words from the Text, 
-            # reset to not lose words (index)
-            wec_text_df = wec_df[["label"]].join(tf).dropna(subset=["term_freq"]).reset_index()
-
-            wec_text_df["doc_id"] = idx
-
-            inverted_index = inverted_index.append(wec_text_df)
-
-    inverted_index = inverted_index.rename(columns={"index":"word"})
-
-    if weight_function == "cfidf":
-        # Get number of documents for each cluster - count unique doc_ids
-        cdf = inverted_index.groupby(["label"])["doc_id"].nunique().rename("df")
-
-        # Get cluster idf values
-        icdf_vector = np.log(N/cdf + 0.01)
-        
-    elif weight_function == "tfidf_sum":
-        # Get term document frequency
-        doc_freq = inverted_index.groupby(["word"])["doc_id"].nunique().rename("df")
-        
-        # Inverse document frequency
-        wec_df["idf"] = np.log(N/doc_freq)
-    
-    wcde_doc_vectors = []
-    
-    for tokenized_text in tokenized_texts:
-
-        if tokenized_text:
-    
-            # Get terms and frequencies from tokenized text
-            tf = pd.Series(tokenized_text, name="term_freq").value_counts()
-            
-            # Filter Word Cluster index (wec_df) to contain only words from the Text, reset to not lose words (index)
-            wec_text_df = wec_df.join(tf).dropna(subset=["term_freq"]).reset_index()
-            
-            if weight_function == "cfidf":            
-                '''
-                # Get cluster frequency - 
-                cf(i,j) = count of every occurrence of any terms of cluster i that are present in document j
-                '''
-                cf_vector = wec_text_df.groupby(["label"])["term_freq"].apply(np.sum)
-                
-                cluster_weights = cf_vector * icdf_vector[cf_vector.index]
-                    
-            elif weight_function == "tfidf_sum":            
-                
-                wec_text_df["tfidf"] = wec_text_df.term_freq * wec_text_df.idf
-                
-                cluster_weights = wec_text_df.groupby(["label"])["tfidf"].apply(np.sum)
-                
-            if bool(normalize): 
-                cluster_weights = cluster_weights/np.sqrt(np.sum(np.square(cluster_weights)))
-                
-            # Re-indexing to include clusters unseen clusters
-            cluster_weights = cluster_weights.reindex(set(cluster_labels), fill_value=0) 
-            
-            wcde_doc_vectors.append(np.asarray(cluster_weights))
-        else:
-            # The text is empty
-            zero_vector = np.zeros(len(wec_df.label.sort_values().unique()))
-            wcde_doc_vectors.append(zero_vector)
-            
-    return wcde_doc_vectors
-
-def get_document_vectors_v2(    
-    tokenized_texts, 
-    words,
-    cluster_labels, 
     training_tokenized_texts=None,
-    weight_function="cfidf", 
     normalize=True
     ):    
+
+    '''
+    Computes the document vectors given word clusters and weight function
+
+    Parameters
+    ----------
+    tokenized_texts : list of lists of str
+        A list of texts where each text is further a list of tokens.        
+    words : list
+        The words that were clustered. 
+    cluster_labels : list
+        For each word, this list gives the label of the cluster it belongs to.
+    weight_function : str, optional
+        Provides the default weighting scheme to be used. The available 
+        options are "cfidf" and "tfidf_sum".
+        The default is "cfidf".
+    training_tokenized_texts : list of list of str, optional
+        The training texts are used to calculate the document frequencies.       
+        The default is None.
+    normalize : bool, optional
+        If True, then the document vectors will be length normalized (L2).
+        The default is True.
+
+    Returns
+    -------
+    labels : list of lists
+        A list of document vector for each documents in the tokenized_texts
+
+    '''
+
      
     if training_tokenized_texts == None:
         training_tokenized_texts = tokenized_texts
@@ -280,6 +209,6 @@ def get_document_vectors_v2(
             if bool(normalize): 
                 cluster_weights = cluster_weights/np.sqrt(np.sum(np.square(cluster_weights)))
             
-        wcde_doc_vectors.append(np.asarray(cluster_weights))
+        wcde_doc_vectors.append(list(np.asarray(cluster_weights)))
     
     return wcde_doc_vectors

demo.py
@@ -59,7 +59,8 @@ def read_bbc_dataset(path):
 
 def read_glove_embeddings(path, vocab, vector_size):
     '''
-    Reads GloVe pre-trained word embeddings into a Pandas Series
+    Reads GloVe pre-trained word embedding and returns returns a list of words 
+    and an array containing word vectors corresponding to the words.
 
     Parameters
     ----------
@@ -72,8 +73,10 @@ def read_glove_embeddings(path, vocab, vector_size):
 
     Returns
     -------
-    embeddings : pandas.Series
-        Pandas Series with words as the index and vector (numpy array) as the corresponding value.
+    words : list
+        Words from the vocab that were found in the word emebedding
+    embeddings : np.array
+        A 2-D of word vectors corresponding to the words
 
     '''
     words_found = []
@@ -88,21 +91,27 @@ def read_glove_embeddings(path, vocab, vector_size):
                 words_found.append(word)
                 embeddings.append(vector) 
                 
-    # Sort Words and Word Evctors
+    # Sort Words and Word Vectors
     words_found, embeddings = zip(*sorted(zip(words_found, embeddings)))
     
-    return list(words_found), np.array(embeddings)
+    embeddings = np.array(embeddings)
+    words_found = list(words_found)
+
+    return words_found, embeddings
         
 if __name__ == "__main__":
 
-    # Read demo variables
-    dataset_path    = "/path/to/bbc"
-    embedding_file  = "/path/to/glove.6B.100d.txt"
+    # Set demo variables
+    dataset_path            = "/path/to/bbc"
+    embedding_file          = "/path/to/glove.6B.100d.txt"
 
     clustering_algorithm    = "ahc"
     linkage                 = "ward"
     n_clusters              = None
     distance_threshold      = 8
+
+    weighting_scheme        = "cfidf"
+    length_normalize        = True
     
     # Read Dataset
     print("Reading dataset.")
@@ -135,8 +144,8 @@ if __name__ == "__main__":
                                                     tokenized_texts,
                                                     words=words,
                                                     cluster_labels=cluster_labels,
-                                                    weight_function="cfidf",
-                                                    normalize=True
+                                                    weight_function=weighting_scheme,
+                                                    normalize=length_normalize
                                                 )
     
     # Task - Cluster Documents